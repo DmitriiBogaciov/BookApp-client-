dcbde23b3004bb123be934d4e2bc2595f67e5a71
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2024-01-31 09:23:34 +0100

    add testRoute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+'use client'
+
+import {useState} from "react";
+
 export default function Home() {
+
+  const [token, setToken] = useState(null);
+
+  const fetchToken = async () => {
+    try {
+      const response = await fetch('api/testRoute');
+      if (response.ok) {
+        const data = await response.json();
+        setToken(data.accessToken);
+      } else {
+        console.log('Error of fetching token');
+      }
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
   return (
     <div className="container flex flex-col justify-content-center align-items-center">
-      <div><p>Main Page</p></div>
+      <div><p>Main Page: {token}</p></div>
+      <button onClick={fetchToken}>TestFetch</button>
       </div>
   )
 }
